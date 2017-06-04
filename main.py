c6740c8958a67c55fa6e5da198702c34fac42f76
Tum <tummytum3@gmail.com>
2017-06-04 15:56:04 -0400

    Define prefix, config file, token

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+# -*- coding: utf-8 -*-
+import configparser
 import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+
+BOT_PREFIX = "ashe-"
+FILE_CONFIG = "config.ini"
+
+client = commands.Bot(command_prefix=BOT_PREFIX)
+config = configparser.SafeConfigParser()
+config.read(FILE_CONFIG)
 
 def main():
-    pass
+    token = config.get("info", "token")
+    client.run(token)
 
 if "__main__" == __name__:
     main()
