2338ad49d569030f4ef7f38f599102eb0afb66df
Tum <tummytum3@gmail.com>
2017-06-04 21:47:29 -0400

    Add basic shutdown command for testing purposes

--- main.py
+++ main.py
@@ -7,13 +7,27 @@ from discord.ext import commands
 BOT_PREFIX = "ashe-"
 FILE_CONFIG = "config.ini"
 
-client = commands.Bot(command_prefix=BOT_PREFIX)
+bot = commands.Bot(command_prefix=BOT_PREFIX, description=":D?", pm_help=True)
 config = configparser.SafeConfigParser()
 config.read(FILE_CONFIG)
 
+TOKEN = config.get("info", "token")
+
+@bot.event
+async def on_ready():
+    print("{} is now online.".format(bot.user.name))
+    print("ID: {}".format(bot.user.id))
+    print("Command prefix: {}".format(BOT_PREFIX))
+
+@bot.command(description="D:", pass_context=True)
+async def shutdown(context):
+    await bot.send_message(context.message.channel, "Shutting down.")
+    await bot.logout()
+
+
 def main():
-    token = config.get("info", "token")
-    client.run(token)
+    bot.run(TOKEN)
+
 
 if "__main__" == __name__:
     main()
