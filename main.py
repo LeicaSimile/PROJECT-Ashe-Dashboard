d2f0719979c6cf732025daf7588f89d56f4a0eec
Tum <tummytum3@gmail.com>
2017-06-05 00:28:22 -0400

    Move all add_cog to a function

--- main.py
+++ main.py
@@ -9,17 +9,19 @@ import settings
 bot = commands.Bot(command_prefix=settings.BOT_PREFIX, description=":D?", pm_help=True)
 
 
-
+def add_commands():
+    bot.add_cog(general.General(bot))
+    bot.add_cog(general.Debugging(bot))
 
 @bot.event
 async def on_ready():
     print("{} is now online.".format(bot.user.name))
     print("ID: {}".format(bot.user.id))
     print("Command prefix: {}".format(settings.BOT_PREFIX))
-    
-    await bot.change_presence(game=discord.Game(name="DDR | {}help".format(settings.BOT_PREFIX)))
-    bot.add_cog(general.General(bot))
 
+    add_commands()
+    await bot.change_presence(game=discord.Game(name="DDR | {}help".format(settings.BOT_PREFIX)))
+    
 
 def main():
     bot.run(settings.TOKEN)
