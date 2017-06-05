f170f80b75e71aa5782113f923942ce52900874c
Tum <tummytum3@gmail.com>
2017-06-05 16:56:20 -0400

    Add logging system

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import discord
 from discord.ext import commands
+import logging
+import logging.config
 
 from bot.commands import general
 from bot.commands import music
@@ -9,17 +11,19 @@ import settings
 
 bot = commands.Bot(command_prefix=settings.BOT_PREFIX, description=":D?", pm_help=None)
 
+logging.config.fileConfig("logging.ini")
+logger = logging.getLogger("main")
 
 def add_commands():
     bot.add_cog(general.General(bot))
     bot.add_cog(general.Debugging(bot))
     bot.add_cog(music.Music(bot))
-
+    
 @bot.event
 async def on_ready():
-    print("{} is now online.".format(bot.user.name))
-    print("ID: {}".format(bot.user.id))
-    print("Command prefix: {}".format(settings.BOT_PREFIX))
+    logger.info("{} is now online.".format(bot.user.name))
+    logger.info("ID: {}".format(bot.user.id))
+    logger.info("Command prefix: {}".format(settings.BOT_PREFIX))
 
     add_commands()
     await bot.change_presence(game=discord.Game(name="DDR | {}help".format(settings.BOT_PREFIX)))
