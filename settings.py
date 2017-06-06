725b0a1c27e3852eb3f31aef30d54cee69e7a933
Tum <tummytum3@gmail.com>
2017-06-05 21:07:31 -0400

    Add constants for placeholders in phrases

--- settings.py
+++ settings.py
@@ -10,3 +10,10 @@ config.read(FILE_CONFIG)
 
 OWNER_ID = config.get("info", "owner-id")
 TOKEN = config.get("info", "token")
+
+"""Placeholders for variables in phrases"""
+DISPLAY_NAME = "%display_name%"
+MENTION = "%mention%"
+SERVER_NAME = "%server%"
+USER_NAME = "%name%"
+
