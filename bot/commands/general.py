5be9279894f0156c0c8e31874beb504be0befe90
Tum <tummytum3@gmail.com>
2017-06-12 18:58:27 -0400

    Use phrases.Database.random_line for shutdown messages

--- bot/commands/general.py
+++ bot/commands/general.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 
 import settings
+import phrases
 
 
 class General():
@@ -9,11 +10,13 @@ class General():
 
     def __init__(self, bot):
         self.bot = bot
+        self.db = phrases.Database(phrases.FILE_DATABASE)
         
     @commands.command(pass_context=True)
     async def shutdown(self, context):
         if context.message.author.id == settings.OWNER_ID:
-            await self.bot.send_message(context.message.channel, "Shutting down.")
+            shutdown_msg = self.db.random_line("phrase", "phrases", {"category_id": phrases.Category.SHUTDOWN.value})
+            await self.bot.send_message(context.message.channel, shutdown_msg)
             await self.bot.logout()
         else:
             await self.bot.send_message(context.message.channel, "Don't tell me what to do.")
