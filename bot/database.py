e217496760b8c3bb411db368e4534ea158b23eec
Tum <tummytum3@gmail.com>
2017-06-24 14:35:33 -0400

    Add DiscordDatabase, BotDatabase classes

--- bot/database.py
+++ bot/database.py
@@ -210,3 +210,60 @@ class Database(object):
             line = c.fetchone()[0]
 
         return line
+
+
+class DiscordDatabase(Database):
+    """ An extension of Database for Discord. """
+
+    def add_server(self, server):
+        """ Adds a server record to the database.
+
+        Args:
+            server(discord.Server): Server to add.
+
+        """
+        pass
+
+    def remove_server(self, server):
+        """ Removes a server from the database.
+
+        Args:
+            server(discord.Server): Server to remove.
+
+        """
+        pass
+
+
+class BotDatabase(DiscordDatabase):
+    """ An extension of DiscordDatabase for functions specific to the bot. """
+
+    def add_song(self, url):
+        """ Adds a song to the database.
+
+        Args:
+            url(str): URL of the song.
+        """
+        pass
+
+    def add_playlist(self, name, user):
+        """ Adds a playlist to the database.
+
+        Playlists are bound to one user across all servers.
+
+        Args:
+            name(str): Name of the playlist.
+            user(discord.Member/User): User who made the playlist.
+
+        """
+        pass
+
+    def add_playlist_song(self, song, playlist):
+        """ Adds a song to a playlist.
+
+        Args:
+            song(): Song to add.
+            playlist(): The target playlist.
+
+        """
+        pass
+    
