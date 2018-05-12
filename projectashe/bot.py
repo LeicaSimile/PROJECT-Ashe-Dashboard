f0dbec04d5ed1e940740c5cda1c1c68ddc2c3e77
Angelica Catalan <leicasimile@gmail.com>
2018-05-12 11:30:47 -0400

    Upgrade discord.py requirement to 0.16.12

--- projectashe/bot.py
+++ projectashe/bot.py
@@ -2,8 +2,6 @@
 import logging
 import logging.config
 
-import discord
-from discord.ext import commands
 import discordion
 
 logging.config.fileConfig("logging.ini")
@@ -11,9 +9,7 @@ logger = logging.getLogger("main")
 
 def main():
     FILE_CONFIG = "settings.ini"
-    description = "For humanity!"
-    bot = commands.Bot(settings.BOT_PREFIX, description=description, pm_help=True)
-    ashe = discordion.Bot(bot, FILE_CONFIG)
+    ashe = discordion.Bot(FILE_CONFIG)
     ashe.run()
 
 
