4155ce3a378e0febab189426b5aa6e82170d79a6
Angelica Catalan <leicasimile@gmail.com>
2019-10-31 00:00:54 -0400

    dfsfdsdfs

--- projectashe/commands/fun.py
+++ projectashe/commands/fun.py
@@ -66,7 +66,7 @@ class Fun(commands.Cog):
         """
         wc_image = wc.to_image()
         b = io.BytesIO()
-        wc_image.save(b, "png")
+        wc_image.save(b, format=wc_image.format)
 
         await report.delete()
         await context.channel.send(f"A wordcloud for {subject}'s past {days} days:", file=discord.File(b.getvalue()))
