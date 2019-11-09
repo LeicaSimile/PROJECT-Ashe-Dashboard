e07c7577ed2e7847abf147c06fae087829a0bdeb
LeicaSimile <leicasimile@gmail.com>
2019-11-08 20:59:49 -0500

    Init dashboard component

---
+++ web/frontend/src/app/client/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  template: '',
+  styles: ['']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
