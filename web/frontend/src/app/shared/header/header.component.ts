16e1ba12152afebc1b0e1c27dbe1acf191f07010
LeicaSimile <leicasimile@gmail.com>
2019-11-09 21:54:19 -0500

    Add header

---
+++ web/frontend/src/app/shared/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `
+    <div class="container">
+      <nav class="navbar navbar-fixed-top">
+        <div class="navbar-header">
+          <a routerLink=""><h1 class="navbar-brand">PROJECT Ashe</h1></a>
+        </div>
+      </nav>
+    </div>
+  `
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
