355bc5b3c87e402dada5144caa24a893ec34d690
LeicaSimile <leicasimile@gmail.com>
2019-11-09 21:56:20 -0500

    Add user service

--- web/frontend/src/app/app.component.ts
+++ web/frontend/src/app/app.component.ts
@@ -2,9 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+  <div class="header">
+    <app-header></app-header>
+  </div>
+  <main class="container-fluid scrollbar">
+    <router-outlet></router-outlet>
+  </main>
+  `, styles: [`
+    main {
+        padding: 1rem;
+        height: 80%;
+        width: 85%;
+        overflow-y: scroll;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'projectashe';
+  title = 'PROJECT Ashe';
 }
