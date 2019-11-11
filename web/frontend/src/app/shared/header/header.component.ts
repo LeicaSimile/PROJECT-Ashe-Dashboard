e7fb3c9052a648ede4aafbdd7e090b013966d2d2
LeicaSimile <leicasimile@gmail.com>
2019-11-10 23:31:54 -0500

    Add login button

--- web/frontend/src/app/shared/header/header.component.ts
+++ web/frontend/src/app/shared/header/header.component.ts
@@ -3,14 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   template: `
-    <nav class="navbar is-fixed-top level has-background-primary" role="navigation" aria-label="main navigation">
+    <nav class="navbar is-fixed-top level" role="navigation" aria-label="main navigation">
       <div class="navbar-brand level-left">
         <div class="level-item">
           <a routerLink=""><h1 class="is-uppercase has-text-light">PROJECT Ashe</h1></a>
         </div>
       </div>
+      <div class="level-right">
+        <div class="level-item">
+          <button class="button is-primary is-outlined">Login</button>
+        </div>
+      </div>
     </nav>
-  `
+  `, styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
