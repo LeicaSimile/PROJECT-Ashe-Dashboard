69cfa9250c8928dbd3d9bff13bb22a04ed809f1f
LeicaSimile <leicasimile@gmail.com>
2019-11-11 23:42:24 -0500

    Add underline hover animation

--- web/frontend/src/app/shared/header/header.component.ts
+++ web/frontend/src/app/shared/header/header.component.ts
@@ -4,10 +4,14 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-header',
   template: `
     <nav class="navbar is-fixed-top level" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand level-left">
-        <div class="level-item">
+      <div class="level-left">
+        <div class="navbar-brand level-item">
           <a routerLink=""><h1 class="is-uppercase has-text-light">PROJECT Ashe</h1></a>
         </div>
+        <a routerLink="" class="level-item">Home</a>
+        <a routerLink="" class="level-item">Features</a>
+        <a routerLink="" class="level-item">Documentation</a>
+        <a routerLink="" class="level-item">Contact</a>
       </div>
       <div class="level-right">
         <div class="level-item">
