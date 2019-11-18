66e19c4395d7be16ed06ac3769659fbb8c3dba76
LeicaSimile <leicasimile@gmail.com>
2019-11-18 09:12:15 -0500

    Delete backend

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 worker: python bot/main.py
+web: npm build
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './client/dashboard/dashboard.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
     HeaderComponent,
     HomeComponent
   ],
