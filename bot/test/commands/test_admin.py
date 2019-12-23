6272f9e939a607006bdfcd0213af020dcf343a34
LeicaSimile <leicasimile@gmail.com>
2019-12-22 23:31:22 -0500

    Add placeholder cases for TestAdmin

---
+++ bot/test/commands/test_admin.py
@@ -0,0 +1,19 @@
+import pytest
+from status import CommandStatus
+import commands.Admin
+
+class TestAdmin:
+    def test_get_inactive_members(self):
+        pass
+
+    def test_purgelist(self):
+        pass
+
+    def test_purgenotify(self):
+        pass
+
+    def test_message(self):
+        pass
+
+    def test_edit(self):
+        pass
