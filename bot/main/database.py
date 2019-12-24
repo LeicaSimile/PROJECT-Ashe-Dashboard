4e7fc6dfedfb64e8b47b7dc82fc18006889cc8ab
LeicaSimile <leicasimile@gmail.com>
2019-12-23 23:46:01 -0500

    Add app schema

--- bot/main/database.py
+++ bot/main/database.py
@@ -5,52 +5,69 @@ def setup():
     try:
         conn = psycopg2.connect(settings.DATABASE_URL, sslmode="require")
         cur = conn.cursor()
-        query = """CREATE TABLE IF NOT EXISTS Inactive_Member (
-            inactive_member_id SERIAL PRIMARY KEY,
-            guild_id INTEGER NOT NULL,
-            member_id INTEGER NOT NULL,
-            last_notified TIMESTAMPTZ NULL,
-            is_exempt BOOLEAN NOT NULL
-        );
+        query = """CREATE SCHEMA IF NOT EXISTS bot
         
-        CREATE TABLE IF NOT EXISTS Default_Config (
+        CREATE TABLE IF NOT EXISTS bot.Default_Config (
             default_config_id SERIAL PRIMARY KEY,
             config_key VARCHAR(32) NOT NULL,
             config_value VARCHAR NULL
         );
 
-        CREATE TABLE IF NOT EXISTS Guild_Config (
+        CREATE TABLE IF NOT EXISTS bot.Guild_Config (
             guild_config_id SERIAL PRIMARY KEY,
             guild_id INTEGER NOT NULL,
             config_key VARCHAR(32) NOT NULL,
             config_value VARCHAR NULL
         );
 
-        CREATE TABLE IF NOT EXISTS Permission (
+        CREATE TABLE IF NOT EXISTS bot.Permission (
             permission_id SMALLINT PRIMARY KEY,
             permission_name VARCHAR(32)
         );
 
-        CREATE TABLE IF NOT EXISTS Guild_Role_Permission (
+        CREATE TABLE IF NOT EXISTS bot.Guild_Role_Permission (
             role_permission_id SERIAL PRIMARY KEY,
             guild_id INTEGER NOT NULL,
             role_id INTEGER NOT NULL,
             permission_level SMALLINT NOT NULL
         );
 
-        CREATE TABLE IF NOT EXISTS Guild_Member_Permission (
+        CREATE TABLE IF NOT EXISTS bot.Guild_Member_Permission (
             member_permission_id SERIAL PRIMARY KEY,
             guild_id INTEGER NOT NULL,
             member_id INTEGER NOT NULL,
             permission_level SMALLINT NOT NULL
         );
+        
+        CREATE TABLE IF NOT EXISTS bot.Inactive_Member (
+            inactive_member_id SERIAL PRIMARY KEY,
+            guild_id INTEGER NOT NULL,
+            member_id INTEGER NOT NULL,
+            last_notified TIMESTAMPTZ NULL,
+            is_exempt BOOLEAN NOT NULL
+        );
         """
+        cur.execute(query)
+        conn.commit()
+    finally:
+        if conn:
+            cur.close()
+            conn.close()
 
-def get_config_value(value):
+def add_config_record(key, value):
+    return
+
+def get_config_value(key):
+    return
+
+def update_config_value(key, value):
+    return
+
+def delete_config_record(key):
     return
 
 def get_inactive_members():
     return
 
 def update_inactive_members():
-    pass
+    return
