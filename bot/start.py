1830d9e98633cf34d2a2c8990ac74f2c977e2689
Angelica Catalan <leicasimile@gmail.com>
2020-02-16 23:48:08 -0500

    Use absolute path for logging config

--- bot/start.py
+++ bot/start.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 import logging
 import logging.config
+import os.path
 import yaml
+from pathlib import Path
 from main.bot import Bot
 
-with open("logging.yaml", "r") as f:
+with open(os.path.join(Path(__file__).parent.absolute(), "logging.yaml"), "r") as f:
     log_config = yaml.safe_load(f.read())
 logging.config.dictConfig(log_config)
 
