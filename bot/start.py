60bc4703ab15e45ee761a0222eb4e25010d1a2cc
Angelica Catalan <leicasimile@gmail.com>
2020-02-16 23:32:48 -0500

    Add yaml logging config

--- bot/start.py
+++ bot/start.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 import logging
+import logging.config
+import yaml
 from main.bot import Bot
 
-logger = logging.getLogger("main")
+with open("logging.yaml", "r") as f:
+    log_config = yaml.safe_load(f.read())
+logging.config.dictConfig(log_config)
 
 def main():
     ashe = Bot(logger=logging.getLogger("bot"))
